48dfc9075cf5ed225ffe83d6e72b1fb6c721b09f
Lavinia Lehaci <lh.lavinia98@gmail.com>
2025-04-09 19:49:55 +0200

    Rename variables, update README.md, and set terrain levels in the scene

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -4,41 +4,41 @@ using UnityEngine.Rendering;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    public Material material;
-    public int xSize = 500;
-    public int zSize = 500;
+    public Material Material;
+    public int XSize = 500;
+    public int ZSize = 500;
     public enum TerrainType
     {
         Elevation,
         Terrace
     }
-    public TerrainType terrainType;
+    public TerrainType Type;
     [Range(1f, 10f)]
-    public float elevationExponent = 3;
+    public float ElevationExponent = 3;
     [Range(1, 32)]
-    public int terraceCount = 20;
-    public Vector2 offset = Vector2.zero;
+    public int TerraceCount = 20;
+    public Vector2 Offset = Vector2.zero;
     [Range(0f, 0.5f)]
-    public float frequency = 0.01f;
+    public float Frequency = 0.01f;
     [Range(1, 10)]
-    public int octaves = 2;
-    public Vector2 heightRange = new Vector2(0, 128);
-    public List<TerrainLevel> terrainLevels = new();
+    public int Octaves = 2;
+    public Vector2 HeightRange = new Vector2(0, 128);
+    public List<TerrainLevel> Levels = new List<TerrainLevel>();
     [System.Serializable]
     public struct TerrainLevel
     {
-        public float minValue;
-        public Color color;
+        public float MinValue;
+        public Color Color;
     }
-    public List<WorldElement> worldElements = new();
+    public List<WorldElement> WorldElements = new List<WorldElement>();
     [System.Serializable]
     public struct WorldElement
     {
-        public GameObject gameObject;
-        public Vector2 spawnLevel;
+        public GameObject GameObject;
+        public Vector2 SpawnLevel;
         [Range(0f, 1f)]
-        public float spawnFrequency;
-        public bool rotateOnNormal;
+        public float SpawnFrequency;
+        public bool RotateOnNormal;
     }
 
     private int _currentXSize;
@@ -67,7 +67,7 @@ public class TerrainGenerator : MonoBehaviour
         MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
         if (meshRenderer == null)
             meshRenderer = gameObject.AddComponent<MeshRenderer>();
-        meshRenderer.material = material;
+        meshRenderer.material = Material;
 
         UpdateTerrain();
     }
@@ -77,21 +77,21 @@ public class TerrainGenerator : MonoBehaviour
         if(_mesh == null)
             return;
 
-        if(xSize < 1 || zSize < 1)
+        if(XSize < 1 || ZSize < 1)
         {
-            xSize = 1;
-            zSize = 1;
+            XSize = 1;
+            ZSize = 1;
             Debug.LogWarning("xSize and zSize must be greater than 0!");
         }
 
-        _currentXSize = xSize;
-        _currentZSize = zSize;
+        _currentXSize = XSize;
+        _currentZSize = ZSize;
 
-        if((uint)(xSize + 1) * (uint)(zSize +1) > uint.MaxValue)
+        if((uint)(XSize + 1) * (uint)(ZSize +1) > uint.MaxValue)
         {
             Debug.LogWarning("Max vertex count exceeded!");
-            xSize = _currentXSize;
-            zSize = _currentZSize;
+            XSize = _currentXSize;
+            ZSize = _currentZSize;
         }
 
         _shouldRegenerateTerrain = true;
@@ -120,36 +120,36 @@ public class TerrainGenerator : MonoBehaviour
         }
         _spawnablesList.Clear();
 
-        _vertices = new Vector3[(xSize + 1) * (zSize + 1)];
-        _vertexHeightsRaw = new float[_vertices.Length];
+        _vertices = new Vector3[(XSize + 1) * (ZSize + 1)];
+        _vertexHeightsRaw = new float[_vertices.Length];     // Stores the height of each vertex before applying calculations according to the type of the terrain.
         _occupiedVertices = new bool[_vertices.Length];
         _colors = new Color[_vertices.Length];
 
         // Vertices
-        for(int vertexIndex = 0, z = 0; z <= zSize; z++)
+        for(int vertexIndex = 0, z = 0; z <= ZSize; z++)
         {
-            for(int x = 0; x <= xSize; x++)
+            for(int x = 0; x <= XSize; x++)
             {
                 float y = 0f;
-                float octaveFrequency = frequency;
-                for(int i = 0; i < octaves; i++)
+                float octaveFrequency = Frequency;
+                for(int i = 0; i < Octaves; i++)
                 {
-                    y += Mathf.PerlinNoise((x + offset.x) * octaveFrequency, (z + offset.y) * octaveFrequency);
+                    y += Mathf.PerlinNoise((x + Offset.x) * octaveFrequency, (z + Offset.y) * octaveFrequency);
                     octaveFrequency *= 2.0f;
                 }
-                y /= octaves;
+                y /= Octaves;
                 _vertexHeightsRaw[vertexIndex] = y;
 
                 float yTerrain;
-                if(terrainType == TerrainType.Terrace)
-                    yTerrain = Mathf.Round(y * terraceCount) / terraceCount;
+                if(Type == TerrainType.Terrace)
+                    yTerrain = Mathf.Round(y * TerraceCount) / TerraceCount;
                 else
-                    yTerrain = Mathf.Pow(y, elevationExponent);
+                    yTerrain = Mathf.Pow(y, ElevationExponent);
 
-                float height = heightRange.x + (yTerrain * (heightRange.y - heightRange.x));
+                float height = HeightRange.x + (yTerrain * (HeightRange.y - HeightRange.x));
                 _vertices[vertexIndex] = new Vector3(x, height, z);
 
-                if(terrainLevels.Count == 0)
+                if(Levels.Count == 0)
                     _colors[vertexIndex] = Color.magenta;
                 else
                     _colors[vertexIndex] = GetColor(y);
@@ -159,18 +159,18 @@ public class TerrainGenerator : MonoBehaviour
         }
         
         // Triangles
-        _triangles = new int[xSize * zSize * 6];
-        for(int vertex = 0, triangleIndex = 0, z = 0; z < zSize; z++)
+        _triangles = new int[XSize * ZSize * 6];
+        for(int vertex = 0, triangleIndex = 0, z = 0; z < ZSize; z++)
         {
-            for(int x = 0; x < xSize; x++)
+            for(int x = 0; x < XSize; x++)
             {
                 _triangles[triangleIndex + 0] = vertex;
-                _triangles[triangleIndex + 1] = vertex + xSize + 1;
+                _triangles[triangleIndex + 1] = vertex + XSize + 1;
                 _triangles[triangleIndex + 2] = vertex + 1;
 
                 _triangles[triangleIndex + 3] = vertex + 1;
-                _triangles[triangleIndex + 4] = vertex + xSize + 1;
-                _triangles[triangleIndex + 5] = vertex + xSize + 2;
+                _triangles[triangleIndex + 4] = vertex + XSize + 1;
+                _triangles[triangleIndex + 5] = vertex + XSize + 2;
 
                 vertex++;
                 triangleIndex += 6;
@@ -184,19 +184,19 @@ public class TerrainGenerator : MonoBehaviour
         int closestHeightIndex = 0;
         float closestHeight = 0f;
 
-        for(int i = 0; i < terrainLevels.Count; i++)
+        for(int i = 0; i < Levels.Count; i++)
         {
-            if(height > terrainLevels[i].minValue)
+            if(height > Levels[i].MinValue)
             {
-                if(terrainLevels[i].minValue > closestHeight)
+                if(Levels[i].MinValue > closestHeight)
                 {
                     closestHeightIndex = i;
-                    closestHeight = terrainLevels[i].minValue;
+                    closestHeight = Levels[i].MinValue;
                 }
             }
         }
 
-        return terrainLevels[closestHeightIndex].color;
+        return Levels[closestHeightIndex].Color;
     }
 
     private void ApplyTerrainData()
@@ -212,28 +212,28 @@ public class TerrainGenerator : MonoBehaviour
 
     private void SpawnWorldElements()
     {
-        if(worldElements.Count == 0)
+        if(WorldElements.Count == 0)
             return;
 
         for(int vertexIndex = 0; vertexIndex < _vertices.Length; vertexIndex++)
         {
-            foreach(WorldElement worldElement in worldElements)
+            foreach(WorldElement worldElement in WorldElements)
             {
                 if(_occupiedVertices[vertexIndex])
                     continue;
 
-                if(_vertexHeightsRaw[vertexIndex] > worldElement.spawnLevel.x && _vertexHeightsRaw[vertexIndex] < worldElement.spawnLevel.y)
+                if(_vertexHeightsRaw[vertexIndex] > worldElement.SpawnLevel.x && _vertexHeightsRaw[vertexIndex] < worldElement.SpawnLevel.y)
                 {
                     float random = Random.Range(0f, 1f);
-                    if(random < worldElement.spawnFrequency)
+                    if(random < worldElement.SpawnFrequency)
                     {
                         Quaternion rotation;
-                        if(worldElement.rotateOnNormal)
+                        if(worldElement.RotateOnNormal)
                             rotation = Quaternion.FromToRotation(transform.up, _mesh.normals[vertexIndex]) * transform.rotation;
                         else
                             rotation = Quaternion.identity;
 
-                        GameObject worldElementObject = Instantiate(worldElement.gameObject, transform.position + _vertices[vertexIndex], transform.rotation * rotation, transform);
+                        GameObject worldElementObject = Instantiate(worldElement.GameObject, transform.position + _vertices[vertexIndex], transform.rotation * rotation, transform);
                         _spawnablesList.Add(worldElementObject);
                         _occupiedVertices[vertexIndex] = true;
                     }
